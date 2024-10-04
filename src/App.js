2730226593b6dfcbe3aab32542b81cd7161b37bf
fulya_ture <fulyature@gmail.com>
2024-10-04 21:59:54 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header";
 import Product from "./components/products/Product";
-import "./App.scss";
+
 import { categories, products } from "./helper/data.js";
 
 function App() {
